fe65e5e7c95ac23a376ee441b5ae49cd28107638
Wojciech Góralczyk <goralczyk4@gmail.com>
2020-11-06 21:23:29 +0100

    Flask running

--- python/rest_with_flask/__init__.py
+++ python/rest_with_flask/__init__.py
@@ -15,15 +15,15 @@ def index():
 #def page(path):
 #    return pages.get_or_404(path).html
 
-@app.route('/delete/<id:int>/')
-def page(id:int):
-    return render_template('page.html', page=page)
-
-@app.route('/list/')
-def page():
+# @app.route('/delete/<id:int>/')
+# def page(id:int):
+#     return render_template('page.html', page=page)
+#
+# @app.route('/list/')
+# def page():
     return render_template('page.html', page=page)
 
 
 if __name__ == '__main__':
     init_db()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
