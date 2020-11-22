d0241e81b6fb5fa00481f567a9b345548becbac3
Wojciech Góralczyk <goralczyk4@gmail.com>
2020-11-22 17:14:48 +0100

    Django app

---
+++ app1/app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import ProductForm
+from app.models import Product
+
+
+def products(request):
+    list = Product.objects.all()
+    return render(request, 'products.html', {'list':list})
+
+def addProduct(request):
+    if request.method == 'POST':
+        formular = ProductForm(request.POST)
+        if formular.is_valid():
+            # add to db
+            return render(request, 'productadded.html')
+    else:
+        formular = ProductForm()
+    return render(request, 'addprodukt.html', {'formular':formular})
