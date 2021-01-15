fc89a3af762259ec93fa89b566a6e7b91eeb77dc
Wojciech Góralczyk <goralczyk4@gmail.com>
2021-01-15 23:06:51 +0100

    Data scrapping works

--- airscrapper/pageScraper.js
+++ airscrapper/pageScraper.js
@@ -4,6 +4,34 @@ const scraperObject = {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}`)
         await page.goto(this.url)
+        await page.waitForSelector(".content > section.schedules > div.schedules > div.schedules-direct > div.schedules-table > div.schedules-body > ul")
+        console.log("Xd")
+        let a = await page.$$eval(".content > section.schedules > div.schedules > div.schedules-direct > div.schedules-table > div.schedules-body > ul", elements => {
+            return elements.map(el => {
+                const fromCity = el.getAttribute("data-departure-name")
+                const fromCountry = el.getAttribute("data-departure-country-name")
+                const toCity = el.getAttribute("data-arrival-name")
+                const toCountry = el.getAttribute("data-arrival-country-name")
+                const arrivalTime = el.getAttribute("data-arrival-time")
+                const availability = el.getAttribute("data-availability")
+                let departureTime = el.querySelector("li.depart > div").textContent.trim()
+                departureTime = departureTime.substring(0, departureTime.indexOf(" "))
+                const airline = el.querySelector("li.airline > div > span").textContent
+                const durationMinutes = el.querySelector("li.duration").getAttribute("data-sort-value")
+                return {
+                    "fromCity": fromCity,
+                    "fromCountry": fromCountry,
+                    "toCity": toCity,
+                    "toCountry": toCountry,
+                    "arrivalTime": arrivalTime,
+                    "availability": availability,
+                    "departureTime": departureTime,
+                    "airline": airline,
+                    "durationMinutes": durationMinutes
+                }
+            })
+        })
+        console.log(a[0])
     }
 }
 
