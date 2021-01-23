fb3414a612938fb847d187f6f844a772646f0edc
Wojciech Góralczyk <goralczyk4@gmail.com>
2021-01-23 21:28:54 +0100

    Test

--- calculator/scritps/script.ts
+++ calculator/scritps/script.ts
@@ -1,10 +1,11 @@
+import {Parser} from "./parser/parser";
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 
-class Calculator {
-    constructor() {
-    }
-}
+const expression = "2+3"
+const result = new Parser(expression).parse()
+window.alert("Result is " + result)
